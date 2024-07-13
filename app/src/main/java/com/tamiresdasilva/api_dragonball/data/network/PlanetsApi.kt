ab6b4b5bdbbb4cd5c5e9a399260b1335407cd57f
tamiresdasilva <tamiresmalvina@rede.ulbra.br>
2024-07-13 09:53:23 -0300

    Adicionar descrição e modificar layout

--- app/src/main/java/com/tamiresdasilva/api_dragonball/data/network/PlanetsApi.kt
+++ app/src/main/java/com/tamiresdasilva/api_dragonball/data/network/PlanetsApi.kt
@@ -3,10 +3,8 @@ package com.tamiresdasilva.api_dragonball.data.network
 import com.tamiresdasilva.api_dragonball.data.network.responses.ItemsResponse
 import retrofit2.http.GET
 
-//É o CharactersApi
 interface PlanetsApi {
     @GET("planets")
 
-    //é o fetchCharacters
     suspend fun fetchPlanets(): ItemsResponse
 }
--- app/src/main/java/com/tamiresdasilva/api_dragonball/data/network/responses/ItemsResponse.kt
+++ app/src/main/java/com/tamiresdasilva/api_dragonball/data/network/responses/ItemsResponse.kt
@@ -3,8 +3,6 @@ package com.tamiresdasilva.api_dragonball.data.network.responses
 import com.google.gson.annotations.SerializedName
 
 data class ItemsResponse(
-    //results vira items
     @SerializedName("items")
-    //charactersResult vira planetsItem e CharactersResponse vira PlanetsResponse
     val planetsItem: List<PlanetsResponse>
 )
